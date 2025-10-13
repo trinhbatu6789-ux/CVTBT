24f5d942d401a03c32137bdcbc74d5696e3fa64a
CVTRINHBATU <trinhbatu6789@gmail.com>
2025-10-13 23:05:03 +0700

    fix(config): update vite base path for github pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/CVTBT/', // Repository name của bạn
+  base: '/', // Để deploy từ master branch
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
