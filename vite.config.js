c890fa6a7795a860b598f8265ab749f0b437916b
CVTRINHBATU <trinhbatu6789@gmail.com>
2025-10-13 22:43:08 +0700

    feat(config): setup github pages deployment with actions

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/CVTBT/', // Repository name của bạn
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    minify: 'esbuild', // Thay đổi từ terser sang esbuild
+  },
 });
